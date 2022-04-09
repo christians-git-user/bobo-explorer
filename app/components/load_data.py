e3a8660daf40d5a4ea25475e89672db4c37162e4
Christian Steckler <christian.steckler@gmail.com>
2022-04-09 12:46:56 -0400

    s3fs

--- app/components/load_data.py
+++ app/components/load_data.py
@@ -1,30 +1,14 @@
 import pyarrow.parquet as pq
 import pandas as pd
-import boto3
 import streamlit as st
+import s3fs
 
 def load_data():
-    s3client = boto3.client(
-        's3',
-        region_name='us-east-1'
-    )
-    
     bucketname = "boboserverstack-boboserviceboboa67afcbb-1vv76sne2db6j" 
     file_to_read = "fileout.txt/part-00000-82ce6fe5-7109-4971-85b9-ce448ad5bc42-c000.snappy.parquet"
 
-    #Create a file object using the bucket and object key. 
-    fileobj = s3client.get_object(
-        Bucket=bucketname,
-        Key=file_to_read
-        ) 
-    st.write(fileobj)
-    # open the file object and read it into the variable filedata. 
-    filedata = fileobj['Body'].read()
-    st.write(filedata)
-
-    arrow_dataset = pq.ParquetDataset('fileobj')
-    arrow_table = arrow_dataset.read()
-    temp = arrow_table.to_pandas()
+    s3 = s3fs.S3FileSystem()
+    temp = pq.read_table(f"s3://{bucketname}/{file_to_read}", filesystem=s3).to_pandas()
 
     st.write(temp)
     
@@ -39,3 +23,5 @@ def load_data():
 
     # groupby
     df_daily = temp.groupby(by='date').count()
+
+    st.write(df_daily)
