7f3ec113ad947064b897e7e462a6e31538ebacc3
Christian Steckler <christian.steckler@gmail.com>
2022-04-09 11:58:39 -0400

    read parquet file from s3

--- app/app.py
+++ app/app.py
@@ -1,47 +1,31 @@
 import streamlit as st
-import os, json
 import pandas as pd
 import numpy as np
-import glob
 import altair as alt
 import time
 
-pd.set_option('display.max_columns', None)
+from components.load_data import load_data
 
-path_to_json = './data' 
-
-
-json_pattern = os.path.join(path_to_json,'*.json')
-file_list = glob.glob(json_pattern)
-
-dfs = [] # an empty list to store the data frames
-for file in file_list:
-    data = pd.read_json(file, lines=True) # read data frame from json file
-    dfs.append(data) # append the data frame to the list
-
-temp = pd.concat(dfs, ignore_index=True) # concatenate all the data frames in the list.
-
-# create day col
-temp['date'] = temp['on'].apply(lambda x: x.split('T')[0])
-
-# groupby
-df_daily = temp.groupby(by='date').count()
+# pd.set_option('display.max_columns', None)
 
 st.title('Bobo Explorer')
 st.warning('EXPERIMENTAL')
 
 st.header('Event Data')
 st.subheader('Raw Table')
-st.write(temp)
 
-st.subheader('Daily Counts')
-st.write(df_daily)
+load_data()
 
-st.subheader('Visualizations')
-st.altair_chart(alt.Chart(df_daily.reset_index()).mark_bar().encode(
-    x='date',
-    y='on'
-))
+# st.write(temp)
+
+# st.subheader('Daily Counts')
+# st.write(df_daily)
+
+# st.subheader('Visualizations')
+# st.altair_chart(alt.Chart(df_daily.reset_index()).mark_bar().encode(
+#     x='date',
+#     y='on'
+# ))
 
 
 
---
+++ app/components/load_data.py
@@ -0,0 +1,31 @@
+# import pandas as pd
+import boto3
+
+def load_data():
+    s3client = boto3.client(
+        's3',
+        region_name='us-east-1'
+    )
+    
+    bucketname = "boboserverstack-boboserviceboboa67afcbb-1vv76sne2db6j" 
+    file_to_read = "fileout.txt/part-00000-82ce6fe5-7109-4971-85b9-ce448ad5bc42-c000.snappy.parquet"
+
+    #Create a file object using the bucket and object key. 
+    fileobj = s3client.get_object(
+        Bucket=bucketname,
+        Key=file_to_read
+        ) 
+    # open the file object and read it into the variable filedata. 
+    filedata = fileobj['Body'].read()
+
+    # file data will be a binary stream.  We have to decode it 
+    contents = filedata.decode('utf-8') 
+
+    # Once decoded, you can treat the file as plain text if appropriate 
+    print(contents)
+
+    # # create day col
+    # temp['date'] = temp['on'].apply(lambda x: x.split('T')[0])
+
+    # # groupby
+    # df_daily = temp.groupby(by='date').count()
